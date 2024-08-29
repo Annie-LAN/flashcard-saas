e651e21a4b2231e989ea1b3bd3db05e41d5e2360
Aining Li <annieli2025@u.northwestern.edu>
2024-08-29 20:40:19 +1200

    add buggy home page + result page

--- app/flashcard/page.js
+++ app/flashcard/page.js
@@ -49,7 +49,7 @@ export default function Flashcard() {
                   <Box
                     sx={
                       {
-                        /* Styling for flip animation */
+                        /* TODO: Styling for flip animation */
                       }
                     }
                   >
